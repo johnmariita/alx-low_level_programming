7c1612130558c94ef2353bc84d936d233c612557
Johnmaestro <johnmariita421@gmail.com>
2023-03-14 10:07:49 +0300

    cat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,10 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	int i = 0, j = 0, m = 0, n = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
