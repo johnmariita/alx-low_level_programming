6dc76cba2f55fdedf4d51085e7585e9230846a4f
Johnmaestro <johnmariita421@gmail.com>
2023-03-16 19:24:00 +0300

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc - function to create memory
+ * @b: size o fthe memory
+ * Return: returns pointer to an address
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *c = malloc(b);
+	return ((void *) c);
+	if (c == NULL)
+		exit (98);
+}
