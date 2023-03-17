9408887f2a2e8b043203770bf6274985a05f1ae2
Johnmaestro <johnmariita421@gmail.com>
2023-03-17 19:12:19 +0300

    setting memory to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * _memset - function to set each memory address to c
+ * @set: memoroy to be allocated whatever you want
+ * @val: what to be entered
+ * @n: how many times
+ * Return: returns address to the memory
+ *
+ */
+char *_memset(char *set, char val, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		set[i] = val;
+	}
+	return (set);
+}
+/**
  * _calloc - calloc function
  * @nmemb: number of blocks
  * @size: size of each block
@@ -14,5 +32,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	calloc = malloc(nmemb * size);
+	_memset(calloc, 0, nmemb * size);
 	return (calloc);
 }
