e22bbfbfd6431950c6ebacae4c0c21dd1950fa3d
Johnmaestro <johnmariita421@gmail.com>
2023-03-21 22:18:23 +0300

    poppy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,18 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("NAME: (nil)\n");
+	{
+		d->name = "(nil)";
+		printf("NAME: %s\n", d->name);
+	}
 	else
 		printf("NAME: %s\n", d->name);
 	printf("AGE: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("OWNER: (nil)\n");
+	{
+		d->owner = "(nil)";
+		printf("OWNER: %s\n", d->owner);
+	}
 	else
 		printf("OWNER: %s\n", d->owner);
 }
