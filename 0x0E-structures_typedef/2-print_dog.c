675ed088bb34c2c1df31dc1ac7128480d2042747
Johnmaestro <johnmariita421@gmail.com>
2023-03-21 04:30:17 +0300

    correction on the print dog function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,12 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("NAME: nil\n");
+		printf("NAME: (nil)\n");
 	else
 		printf("NAME: %s\n", d->name);
 	printf("AGE: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("OWNER: nil\n");
+		printf("OWNER: (nil)\n");
 	else
 		printf("OWNER: %s\n", d->owner);
 }
