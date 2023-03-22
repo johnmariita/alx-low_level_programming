b2c97dc7424ad694d12b05f2c5f82df1b57590b8
Johnmaestro <johnmariita421@gmail.com>
2023-03-22 23:05:33 +0300

    func pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,6 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int result;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+
 	for (i = 0; i < size; i++)
 	{
 		result = cmp(array[i]);
