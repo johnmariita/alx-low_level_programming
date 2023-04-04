0a3d61c698c3c09f1364c8cbf09e5e80aa28bb9f
Johnmaestro <johnmariita421@gmail.com>
2023-04-04 23:41:04 +0300

    pprint list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint - function to print the nodes
+ * @h: pointer to the structure of the linked list
+ * Return: returns the count of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int node_count = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		node_count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (node_count);
+}
