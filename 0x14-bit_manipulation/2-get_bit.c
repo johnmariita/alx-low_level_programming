28f1820c057130fdb25eeab7ebe8e73c1f4ef052
Johnmaestro <johnmariita421@gmail.com>
2023-05-05 16:44:24 +0300

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * powr - function to get the power
+ * @base: the base
+ * @exp: the exponent
+ * Return: returns the power
+ *
+ */
+int powr(int base, int exp)
+{
+	if (exp == 0)
+		return (1);
+	return (base * powr(base, exp - 1));
+}
+/**
+ * get_bit - function to get the bit at a particular index
+ * @n: the integer whose indexth bit we want
+ * @index: the index at which we want the bit
+ * Return: returns 1 if the bit is 1 and 0 if it's 0
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((n & powr(2, index)) > 0)
+		return (1);
+	else
+		return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <limits.h>
+int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
