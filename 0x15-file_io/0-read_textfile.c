cdc86336cc6a38cdfda9ab23e9aa8c307e076e33
Johnmaestro <johnmariita421@gmail.com>
2023-05-08 17:30:22 +0300

    file handling

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file to the stdout
+ * @filename: the file being read from
+ * @letters: the letters to be read
+ * Return: returns the no. of bytes read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf = malloc(letters);
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	int fd;
+
+	if (filename == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == EOF)
+	{
+		free(buf);
+		return (0);
+	}
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	bytes_written = write(1, buf, letters);
+	if (bytes_written < 0 || bytes_written < bytes_read)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (bytes_read);
+	close(fd);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+##FILE I/O
+Creating, opening, reading, writing and closing files
