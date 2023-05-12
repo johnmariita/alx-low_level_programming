92fada025952ec50d7b997a06c247069227a8a2a
Johnmaestro <johnmariita421@gmail.com>
2023-05-12 23:53:48 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,67 +2,73 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define BUFFSIZE 1024
 /**
- * copy - a function that copies a file content to another file
- * @file_from: file being copied
- * @file_to: file_being copied to
+ * read_file_opener - function to open a file
+ * @filename: file to be opened
+ * Return: returns the file descriptor
  */
-void copy(char *file_from, char *file_to)
+ssize_t read_file_opener(char *filename)
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buf[BUFFSIZE];
+	ssize_t fd_open = open(filename, O_RDONLY);
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from != -1)
-		bytes_read = read(fd_from, buf, BUFFSIZE);
-	else
+	if (fd_open == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		dprintf(2, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-	if (bytes_read == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(2, "ERROR: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	bytes_written = write(fd_to, buf, bytes_read);
-	if (bytes_written == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	return (fd_open);
 }
 /**
- * main - main function
- * @argc: argument count
- * @argv: argument vector
+ * write_file_opener - function that writes to a file
+ * @filename: the filename to be written to
+ * Return: returns the file descriptor
+ */
+ssize_t write_file_opener(char *filename)
+{
+	ssize_t fd_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd_open == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (fd_open);
+}
+/**
+ * main - the main function
+ * @argc: the argument count
+ * @argv: the argument vector
  * Return: returns 0 on success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	ssize_t from, to, bytes_read;
+	ssize_t bytes_written;
+	int tot_read = 0;
+	char buffer[1024];
+
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	copy(argv[1], argv[2]);
-	return (100);
+	from = read_file_opener(argv[1]);
+	while (bytes_read > 0)
+	{
+		bytes_read = read(from, buffer, 1024);
+		tot_read += bytes_read;
+	}
+	to = write_file_opener(argv[2]);
+	bytes_written = write(to, buffer, tot_read);
+	if (close(from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %li\n", from);
+		exit(100);
+	}
+	if (close(to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %li\n", to);
+		exit(100);
+	}
+	return (bytes_written);
 }
