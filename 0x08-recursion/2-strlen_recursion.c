33fdfc39ef81a00acc935f2ae08370c240936ee2
johnmariita <johnmariita421@gmail.com>
2023-06-07 17:21:02 +0300

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	static int count;
-
-	if (*s != '\0')
-	{
-		_strlen_recursion(s + 1);
-		count++;
-	}
-	return (count);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
