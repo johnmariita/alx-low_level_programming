db30500f86dfe7171e70708bda7ebe1b4c81ce40
johnmariita <johnmariita421@gmail.com>
2023-06-10 17:46:40 +0300

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 0 on success
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
