5be48d29db0780047341dae0de10a63fd25ebf5a
johnmariita <johnmariita421@gmail.com>
2023-06-11 01:10:44 +0300

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * myatoi - function to convert string to integer
+ * @str: the string
+ * Return: returns the converted number
+ */
+int myatoi(char *str)
+{
+	int num = 0, i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			printf("Error\n");
+			exit(1);
+		}
+		num *= 10;
+		num += str[i] - '0';
+		i++;
+	}
+	return (num);
+}
+
+/**
+ * main - the main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0, i = 1;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (argv[i])
+	{
+		sum += myatoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
