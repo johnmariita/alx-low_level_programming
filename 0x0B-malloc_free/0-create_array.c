6cfa2a8cf21f0496a966a124907847eccb2a113b
johnmariita <johnmariita421@gmail.com>
2023-06-12 15:53:48 +0300

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i = 0;
 
+	if (size <= 0)
+		return (NULL);
 	array = malloc(sizeof(char) * size);
 	if (!array)
 		return (NULL);
