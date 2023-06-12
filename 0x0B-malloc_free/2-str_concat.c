32146293c22e4f3882e5dcd1deacffba3e0c307a
johnmariita <johnmariita421@gmail.com>
2023-06-12 17:15:51 +0300

    fixed the NULL case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,14 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0;
 
 	if (s1 == NULL && s2 == NULL)
-		return ("");
+	{
+		s1 = "";
+		s2 = "";
+	}
 	if (s1 == NULL)
-		return (s2);
+		s1 = "";
 	if (s2 == NULL)
-		return (s1);
+		s2 = "";
 	while (s1[count])
 		count++;
 	while (s2[i])
