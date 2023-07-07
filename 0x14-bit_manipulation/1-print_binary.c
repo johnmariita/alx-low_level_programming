da460557c39ed195d35d86160dd59a35081a9f06
johnmariita <johnmariita421@gmail.com>
2023-07-07 14:40:21 +0300

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,32 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	char b[1024];
-	int i = 0, count = 0, x = (int)n;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (x > 0)
-	{
-		b[i] = ((x & 1) + '0');
-		x >>= 1;
-		i++;
-	}
-	b[i] = '\0';
-	i = 0;
-	while (b[i])
-	{
-		count++;
-		i++;
-	}
-	i = 0;
-	count -= 1;
-	while (count >= 0)
-	{
-		_putchar(b[count]);
-		count--;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
