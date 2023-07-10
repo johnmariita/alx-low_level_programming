922794358276715195fb1534e5cbf71a19bddac9
johnmariita <johnmariita421@gmail.com>
2023-07-10 15:28:16 +0300

    read files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a file
+ * @filename: the path to the file
+ * @letters: count of letters
+ * Return: returns the number of letters read or 0 upon failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd;
+	char buffer[1024];
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	rd = read(fd, buffer, letters);
+	if (rd < 0)
+		return (0);
+	write(1, buffer, rd);
+	if (close(fd) < 0)
+		return (0);
+	return (rd);
+}
