6941b6c2ce2f184b8d44aa388dda099c75967893
johnmariita <johnmariita421@gmail.com>
2023-07-10 16:17:21 +0300

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,20 +13,37 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, rd, written;
-	char buffer[1024];
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
+	buffer = malloc(letters);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+	{
+		free(buffer);
 		return (0);
+	}
 	rd = read(fd, buffer, letters);
 	if (rd < 0)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 	written = write(1, buffer, rd);
 	if (written != rd)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 	if (close(fd) < 0)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
+	free(buffer);
 	return (written);
 }
