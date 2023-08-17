cb7943a6c20c02b944dbc7ac3fc3e0f8501d1dcf
johnmariita <johnmariita421@gmail.com>
2023-08-17 22:55:26 +0300

    largest palindrom number

---
+++ 0x17-doubly_linked_lists/largest_palindrom.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def is_palindrome(num):
+    temp = num
+    new_num = 0
+    while temp:
+        new_num *= 10
+        new_num += temp % 10
+        temp //= 10
+    return True if new_num == num else False
+
+print("palindrome") if is_palindrome(2002) else print("not a palindrome")
+largest = 0
+for i in range(101, 1000):
+    for j in range(101, 1000):
+        if (is_palindrome(i * j)):
+            if i * j > largest:
+                largest = i * j
+
+print(largest)
