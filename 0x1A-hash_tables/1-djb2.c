eb29228e4e0c74b6712e5c847c496ea0ffcc588b
johnmariita <johnmariita421@gmail.com>
2023-09-07 21:26:40 +0300

    hashing function

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - a hashing function
+ * @str: the string to be hashed
+ * Return: returns the hash code of the string
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
