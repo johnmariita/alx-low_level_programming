662ca45943828d32cd6b5d8b3c99760245bc9fb5
johnmariita <johnmariita421@gmail.com>
2023-09-07 21:31:32 +0300

    key index in the hash map

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * key_index - function that returns the index of a key in the hash map
+ * @key: the key
+ * @size: size of the array
+ * Return: returns the index of the key in the array
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_code = hash_djb2(key) % size;
-	return hash_code;
+
+	return (hash_code);
 }
